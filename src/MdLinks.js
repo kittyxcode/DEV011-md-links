54be50a2a60765eeb334de0382c31c3414974465
kittyxcode <katherine.pino059@gmail.com>
2023-11-17 02:55:06 -0300

    Crear mdLinks Function, testing mdLinks, install JEST

---
+++ src/MdLinks.js
@@ -0,0 +1,4 @@
+//crear fuincion mdLinks
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {});
+};
---
+++ test/mdLinks.spec.js
@@ -0,0 +1,13 @@
+const mdLinks = require("../src/mdLinks");
+
+describe("mdLinks", () => {
+  it("should...", () => {
+    console.log("FIX ME!");
+  });
+});
+
+describe("mdLinks", () => {
+  it("deberia retornar una promesa", () => {
+    expect()
+  });
+});
