1f7a6c7e03007d0b06f01523c49da0e45192106b
kittyxcode <katherine.pino059@gmail.com>
2023-11-28 19:28:55 -0300

    prueba de concepto con process.argv

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,16 @@
+#!/usr/bin/env node
+
 //Cli.js recibe argumentos por consola y los pasa a mdLinks
 
 const mdLinks = require("./mdLinks");
+
+const args = process.argv.slice(2);
+const filePath = args[0];
+const options = args.slice(1);
+
+console.log("ruta: " + filePath);
+console.log("option: " + options);
+console.log("args: " + args);
 
 mdLinks("/Users/kathzy/Documents/Codes/DEV011-md-links/test/fileTest.md")
   .then((links) => {
